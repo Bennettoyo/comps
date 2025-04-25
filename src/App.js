9841a1780043718a966a7f57f379ae16d49f70f7
Jack Bennett <59575631+Bennettoyo@users.noreply.github.com>
2025-04-25 09:26:11 +0100

    react-icons added, tailwind classes added

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
 import Button from "./Button";
+import { GoBell, GoCloudDownload, GoDatabase } from "react-icons/go";
 
 function App() {
   return (
     <div>
       <div>
         <Button success rounded outline>
+          <GoBell />
           Click me!!
         </Button>
       </div>
       <div>
         <Button danger outline>
+          <GoCloudDownload />
           Buy now
         </Button>
       </div>
       <div>
-        <Button warning>See Deal</Button>
+        <Button warning>
+          <GoDatabase />
+          See Deal
+        </Button>
       </div>
       <div>
         <Button secondary outline>
