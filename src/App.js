8cad13ad4be1b440525b312903b04afffcc5246c
Jack Bennett <59575631+Bennettoyo@users.noreply.github.com>
2025-04-29 14:37:58 +0100

    Adding ids to item list, adding accordion component

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import Accordion from "./components/Accordion";
 function App() {
   const items = [
     {
+      id: "dwfwqfqwf",
       label: "Can I use React on a project",
       content:
         "You can use React on any project you want. You can use React on any project you want. You can use React on any project you want.",
     },
     {
+      id: "dwdwdwvvdvd",
       label: "Can I use Javascript on a project",
       content:
         "You can use React on any project you want. You can use React on any project you want. You can use React on any project you want.",
     },
     {
+      id: "codckodco",
       label: "Can I use CSS on a project",
       content:
         "You can use React on any project you want. You can use React on any project you want. You can use React on any project you want.",
--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,5 +1,22 @@
+import { useState } from "react";
+
 function Accordion({ items }) {
-  return <div></div>;
+  const [expandedIndex, setExpandedIndex] = useState(0);
+
+  const renderedItems = items.map((item, index) => {
+    const isExpanded = index === expandedIndex;
+
+    console.log(isExpanded);
+
+    return (
+      <div key={item.id}>
+        <div>{item.label}</div>
+        <div>{item.content}</div>
+      </div>
+    );
+  });
+
+  return <div>{renderedItems}</div>;
 }
 
 export default Accordion;
